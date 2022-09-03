0b877780c2ed5a7f3f86d2f50b917d0abebbef9c
octobercold <theminddepth@gmail.com>
2022-09-03 21:23:05 +0100

    fix registration and add codegen

--- lireddit-server/src/resolvers/user.ts
+++ lireddit-server/src/resolvers/user.ts
@@ -1,138 +1,176 @@
 import "reflect-metadata";
 import { MyContext } from "src/types";
-import { Resolver, Mutation, Arg, InputType, Field, Ctx, Query, ObjectType } from "type-graphql";
+import {
+    Resolver,
+    Mutation,
+    Arg,
+    InputType,
+    Field,
+    Ctx,
+    Query,
+    ObjectType,
+} from "type-graphql";
 import { User } from "../entities/User";
-import argon2 from "argon2"
+import argon2 from "argon2";
+import { EntityManager } from "@mikro-orm/postgresql";
 
 @InputType()
 class UsernamePasswordInput {
     @Field()
-    username: string
+    username: string;
     @Field()
-    password: string
+    password: string;
 }
 
 @ObjectType()
 class FieldError {
     @Field()
-    field: string
-    
+    field?: string;
+
     @Field()
-    message: string
+    message: string;
 }
 
 @ObjectType()
 class UserResponse {
-    @Field(() => [FieldError], {nullable: true})
-    errors?: FieldError[]
+    @Field(() => [FieldError], { nullable: true })
+    errors?: FieldError[];
 
-    @Field(() => User, {nullable: true})
-    user?: User
+    @Field(() => User, { nullable: true })
+    user?: User;
 }
 
 @Resolver()
 export class UserResolver {
     @Query(() => User, { nullable: true })
-    async me(
-        @Ctx() { req, em }: MyContext
-    ) {
+    async me(@Ctx() { req, em }: MyContext) {
         if (!req.session.userId) {
-            return  null
+            return null;
         }
-        const user = await em.findOne(User, {id: req.session.userId})
-        return user
+        const user = await em.findOne(User, { id: req.session.userId });
+        return user;
     }
 
     @Query(() => [User])
     users(@Ctx() { em }: MyContext): Promise<User[]> {
-            return em.find(User, {});
+        return em.find(User, {});
     }
 
     @Mutation(() => UserResponse)
     async register(
-        @Arg('options') options: UsernamePasswordInput,
+        @Arg("options") options: UsernamePasswordInput,
         @Ctx() { em, req }: MyContext
     ): Promise<UserResponse> {
         if (options.username.length <= 2) {
             return {
-                errors: [{
-                    field: "username",
-                    message: "length must be greate than 2"
-                }]
-            }
+                errors: [
+                    {
+                        field: "username",
+                        message: "length must be greate than 2",
+                    },
+                ],
+            };
         }
         if (options.password.length <= 2) {
             return {
-                errors: [{
-                    field: "password",
-                    message: "length must be greate than 2"
-                }]
-            }
+                errors: [
+                    {
+                        field: "password",
+                        message: "length must be greate than 2",
+                    },
+                ],
+            };
         }
-        const hashedPassword = await argon2.hash(options.password)
-        const user = em.create(User, { username: options.username, password:hashedPassword })
+        const hashedPassword = await argon2.hash(options.password);
+        let user;
         try {
-            await em.persistAndFlush(user)
+            const result = await (em as EntityManager)
+                .createQueryBuilder(User)
+                .getKnexQuery()
+                .insert({
+                    username: options.username,
+                    password: hashedPassword,
+                    created_at: new Date(),
+                    updated_at: new Date(),
+                })
+                .returning("*");
+            user = result[0];
+            // await em.persistAndFlush(user);
         } catch (err) {
             //duplicate username error
-            if (err.code === "23505") {
+            if (err.detail.includes("already exists")) {
                 return {
-                    errors: [{
-                        field: "username",
-                        message: "username has already been taken"
-                    }]
-                }
+                    errors: [
+                        {
+                            field: "username",
+                            message: "username has already been taken",
+                        },
+                    ],
+                };
+            } else {
+                return {
+                    errors: [
+                        {
+                            message: "unknown error",
+                        },
+                    ],
+                };
             }
         }
-        req.session.userId = user.id
+        req.session.userId = user.id;
         return { user };
     }
 
     @Mutation(() => UserResponse)
     async deleteUser(
-        @Arg('id') id: number,
+        @Arg("id") id: number,
         @Ctx() { em }: MyContext
     ): Promise<UserResponse> {
-        const user = await em.findOne(User, { id })
+        const user = await em.findOne(User, { id });
         if (!user) {
             return {
-                errors: [{
-                    field: "id",
-                    message: "use with that id doesn't exist"
-                }],
-            }
+                errors: [
+                    {
+                        field: "id",
+                        message: "use with that id doesn't exist",
+                    },
+                ],
+            };
         }
-        await em.nativeDelete(User, {id})
-        return { user }
+        await em.nativeDelete(User, { id });
+        return { user };
     }
 
     @Mutation(() => UserResponse)
     async login(
-        @Arg('options') options: UsernamePasswordInput,
+        @Arg("options") options: UsernamePasswordInput,
         @Ctx() { em, req }: MyContext
     ): Promise<UserResponse> {
-        const user = await em.findOne(User, { username: options.username })
+        const user = await em.findOne(User, { username: options.username });
         if (!user) {
             return {
-                errors: [{
-                    field: "username",
-                    message: "that username doesn't exist"
-                }],
-            }
+                errors: [
+                    {
+                        field: "username",
+                        message: "that username doesn't exist",
+                    },
+                ],
+            };
         }
-        const valid = await argon2.verify(user.password, options.password)
+        const valid = await argon2.verify(user.password, options.password);
         if (!valid) {
             return {
-                errors: [{
-                    field: "password",
-                    message: "incorrect password",
-                }]
-            }
+                errors: [
+                    {
+                        field: "password",
+                        message: "incorrect password",
+                    },
+                ],
+            };
         }
 
-        req.session.userId = user.id
+        req.session.userId = user.id;
 
-        return { user }
+        return { user };
     }
 }
-

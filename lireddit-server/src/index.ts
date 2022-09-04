acf5fa7aab04486167baa388010f798ebf33d8af
octobercold <theminddepth@gmail.com>
2022-09-04 19:07:27 +0100

    add logout functionality

--- lireddit-server/src/index.ts
+++ lireddit-server/src/index.ts
@@ -1,5 +1,5 @@
-import "reflect-metadata"
-import { ___prod___ } from "./constants";
+import "reflect-metadata";
+import { COOKIE_NAME, ___prod___ } from "./constants";
 import { MikroORM } from "@mikro-orm/core";
 import microConfig from "./mikro-orm.config";
 import express from "express";
@@ -8,15 +8,11 @@ import { buildSchema } from "type-graphql";
 import { HelloResolver } from "./resolvers/hello";
 import { PostResolver } from "./resolvers/post";
 import { UserResolver } from "./resolvers/user";
-import Redis from 'ioredis'
-import session from 'express-session'
-import connectRedis from 'connect-redis'
-import {
-    ApolloServerPluginLandingPageGraphQLPlayground
-  } from "apollo-server-core";
-import cors from "cors"
- 
-
+import Redis from "ioredis";
+import session from "express-session";
+import connectRedis from "connect-redis";
+import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
+import cors from "cors";
 
 const main = async () => {
     const orm = await MikroORM.init(microConfig);
@@ -24,43 +20,45 @@ const main = async () => {
     const emFork = orm.em.fork();
     await orm.getMigrator().up();
 
-    const RedisStore = connectRedis(session)
-    const redis = new Redis()
+    const RedisStore = connectRedis(session);
+    const redis = new Redis();
 
     const app = express();
-    app.use(cors({
-        origin: "http://localhost:3000",
-        credentials: true
-    })) 
+    app.use(
+        cors({
+            origin: "http://localhost:3000",
+            credentials: true,
+        })
+    );
 
     app.use(
         session({
-            name: 'qid',
-            store: new RedisStore({ client: redis, disableTouch: true}),
+            name: COOKIE_NAME,
+            store: new RedisStore({ client: redis, disableTouch: true }),
             cookie: {
-                maxAge: 1000*60*60*24*365*10,
+                maxAge: 1000 * 60 * 60 * 24 * 365 * 10,
                 httpOnly: true,
-                sameSite: 'lax',
+                sameSite: "lax",
                 secure: ___prod___,
             },
-            secret: 'skdjfljnuinejskandf',
+            secret: "skdjfljnuinejskandf",
             resave: false,
-            saveUninitialized: false
+            saveUninitialized: false,
         })
-    )
+    );
 
     const apolloServer = new ApolloServer({
         schema: await buildSchema({
             resolvers: [HelloResolver, PostResolver, UserResolver],
             validate: false,
         }),
-        context: ({req, res}) => ({ em: emFork, req, res }),
-        plugins: [ApolloServerPluginLandingPageGraphQLPlayground()]
+        context: ({ req, res }) => ({ em: emFork, req, res }),
+        plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
     });
 
     await apolloServer.start();
 
-    apolloServer.applyMiddleware({ app, cors: false});
+    apolloServer.applyMiddleware({ app, cors: false });
 
     app.listen(4000, () => {
         console.log("server started on localhost: 4000");
--- lireddit-web/src/components/NavBar.tsx
+++ lireddit-web/src/components/NavBar.tsx
@@ -1,9 +1,10 @@
 import { Box, Button, Flex, Link } from "@chakra-ui/react";
 import React from "react";
 import NextLink from "next/link";
-import { useMeQuery } from "../generated/graphql";
+import { useLogoutMutation, useMeQuery } from "../generated/graphql";
 
 export const NavBar: React.FC = () => {
+    const [{ fetching: logoutFetching }, logout] = useLogoutMutation();
     const [{ data, fetching }] = useMeQuery();
     let body = null;
     if (fetching) {
@@ -23,7 +24,15 @@ export const NavBar: React.FC = () => {
         body = (
             <Flex>
                 <Box mr={2}>{data.me.username}</Box>
-                <Button variant="link">logout</Button>
+                <Button
+                    onClick={() => {
+                        logout({});
+                    }}
+                    isLoading={logoutFetching}
+                    variant="link"
+                >
+                    logout
+                </Button>
             </Flex>
         );
     }

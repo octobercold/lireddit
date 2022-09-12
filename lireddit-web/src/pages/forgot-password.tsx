79c489904664ea72c89583edd592694985766f5b
octobercold <theminddepth@gmail.com>
2022-09-12 21:54:36 +0100

    fix forgot password bug

--- lireddit-web/src/pages/forgot-password.tsx
+++ lireddit-web/src/pages/forgot-password.tsx
@@ -1,7 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
+import { Formik, Form } from "formik";
+import Wrapper from "../components/Wrapper";
+import InputField from "../components/InputField";
+import { Box, Button } from "@chakra-ui/react";
+import { useForgotPasswordMutation } from "../generated/graphql";
+import { toErrorMap } from "../utils/toErrorMap";
+import { useRouter } from "next/router";
+import { withUrqlClient } from "next-urql";
+import { createUrqlClient } from "../utils/createUrqlClient";
+import NextLink from "next/link";
 
 const ForgotPassword: React.FC = () => {
-    return <div>Forgot password page</div>;
+    const [complete, setComplete] = useState(false);
+    const [, forgotPassword] = useForgotPasswordMutation();
+    return (
+        <Wrapper variant="small">
+            <Formik
+                initialValues={{ email: "" }}
+                onSubmit={async (values) => {
+                    await forgotPassword(values);
+                    setComplete(true);
+                }}
+            >
+                {({ isSubmitting }) =>
+                    complete ? (
+                        <Box>
+                            if an account with that email exists, we sent you
+                            email
+                        </Box>
+                    ) : (
+                        <Form>
+                            <InputField
+                                name="email"
+                                placeholder="email"
+                                label="Email"
+                                type="email"
+                            />
+                            <Button
+                                mt={4}
+                                type="submit"
+                                colorScheme="teal"
+                                isLoading={isSubmitting}
+                            >
+                                forgot password
+                            </Button>
+                        </Form>
+                    )
+                }
+            </Formik>
+        </Wrapper>
+    );
 };
 
-export default ForgotPassword;
+export default withUrqlClient(createUrqlClient, { ssr: false })(ForgotPassword);

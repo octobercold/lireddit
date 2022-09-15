e0ac3ddcfd55d6af4a195e77bd5752845f928198
octobercold <theminddepth@gmail.com>
2022-09-15 22:58:22 +0100

    add global error handling

--- lireddit-web/src/components/NavBar.tsx
+++ lireddit-web/src/components/NavBar.tsx
@@ -39,7 +39,7 @@ const NavBar: React.FC = () => {
     }
 
     return (
-        <Flex bg="tan" p={4}>
+        <Flex zIndex={1} position="sticky" top={0} bg="tan" p={4}>
             <Box ml={"auto"}>{body}</Box>
         </Flex>
     );
